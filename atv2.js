550131de191773f3d1239e4c757968c64b4e7e96
Maria Eduarda <dudadeandrade13@gmail.com>
2025-05-30 14:48:24 -0300

    atv 2

---
+++ atv2.js
@@ -0,0 +1,13 @@
+function Par(numero) {
+    if (numero % 2 == 0) {
+        return "true"
+    } else {
+        return "false"
+    }
+}
+
+console.log(Par(69));
+console.log(Par(89));
+console.log(Par(80));
+console.log(Par(42));
+console.log(Par(57));
